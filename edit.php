49daf1b89df884d10fb8f2b0dd492ecd1b3bfe84
Jordan Korley-Myron <jordan.korley-myron@ucc.on.ca>
2017-05-03 12:25:22 -0400

    pretty

--- edit.php
+++ edit.php
@@ -1,11 +1,7 @@
 <html>
 
 	<head>
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		
 
 		<!--Import Google Icon Font-->
 	    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -58,66 +54,38 @@
 			  }
 			}
 		</script>
-
-		<nav class="navbar navbar-inverse navbar-static-top">
-		  <div class="container-fluid">
-		    <!-- Brand and toggle get grouped for better mobile display -->
-		    <div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		      <a class="navbar-brand" href="#">Brand</a>
-		    </div>
-
-		    <!-- Collect the nav links, forms, and other content for toggling -->
-		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-		      <ul class="nav navbar-nav">
-		        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-		        <li><a href="#">Link</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li role="separator" class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		            <li role="separator" class="divider"></li>
-		            <li><a href="#">One more separated link</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		      <form class="navbar-form navbar-left">
-		        <div class="form-group">
-		          <input type="text" class="form-control" placeholder="#Search" id="filter" onkeyup="myFunction()">
-		        </div>
-		      </form>
-		      <ul class="nav navbar-nav navbar-right">
-		        <li><a href="#">Link</a></li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li role="separator" class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		          </ul>
-		        </li>
-		      </ul>
-		    </div><!-- /.navbar-collapse -->
-		  </div><!-- /.container-fluid -->
-		</nav>
-
+		   
+			<nav class="nav-extended">
+			    <div class="nav-wrapper">
+					
+			    	<a href="#" class="brand-logo">Logo</a>
+			    	<ul id="nav-mobile" class="right hide-on-med-and-down">
+			        	<li><a href="sass.html">Sass</a></li>
+			        	<li><a href="badges.html">Components</a></li>
+			        	<li><a href="collapsible.html">JavaScript</a></li>
+			    	</ul>
+				</div>
+				<div class="nav-wrapper">
+      				<form>
+        				<div class="input-field">
+          					<input id="search" type="search" required>
+          					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          					<i class="material-icons">close</i>
+        				</div>
+      				</form>
+    			</div>
+			</nav>
 	</head>
 	<body>
     <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
      
+    
+      <div class="input-field col s6">
+          <input id="filter" type="text" class="validate">
+          <label for="filter">#Search</label>
+      </div>
 
 	<?php
 	
